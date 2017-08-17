117cc9632619fb5ed08187a45c25e179f5add5c9
Waleed-Ahmed94 <chwaleedjajja@gmail.com>
2017-08-17 18:05:28 +0500

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,25 +7,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
 from usermanager.models import PollUser
-#from django.views import generic
-
-# Create your views here.
-#class IndexView(generic.ListView):
-    #template_name = 'polls/index.html'
-    #context_object_name = 'latest_question_list'
-    #def get_queryset(self):
-        #"""Return the last five published questions."""
-        #return Question.objects.order_by('-pub_date')[:5]
-
-
-#class DetailView(generic.DetailView):
-    #model = Question
-    #template_name = 'polls/detail.html'
-
-
-#class ResultsView(generic.DetailView):
-    #model = Question
-    #template_name = 'polls/results.html'
 
 def login_required(func):
     def function_wrapper(request, **args):
